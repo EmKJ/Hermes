e3b7622f2d00dc3cf2bf8430862cbf8326ea1bb1
EmKJ <ekjacksondev@outlook.com>
2023-10-15 20:38:34 -0500

    generated starting components

---
+++ ui-hermes/src/app/api-table/api-table.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-api-table',
+  templateUrl: './api-table.component.html',
+  styleUrls: ['./api-table.component.css']
+})
+export class ApiTableComponent {
+
+}
